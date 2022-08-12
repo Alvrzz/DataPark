65bc4ab31c48a6efe53dfead17f86ac837ba68ac
William Justino <104076998+Matrix0112358@users.noreply.github.com>
2022-08-12 11:11:31 -0300

    Resultado da temperatura foi mudada para apenas um decimal

--- previsao.py
+++ previsao.py
@@ -3,21 +3,21 @@ import datetime
 
 # link para abrir open_weather: https://openweathermap.org/
 
-now = datetime.datetime.now()
-hour = now.hour
 API_KEY = "241e22adb42697eaee0b5e8e6945d09a"
+agora = datetime.datetime.now()
+hora = agora.hour
 cidade = "Blumenau"
 link = f"https://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={API_KEY}&lang=pt_br"
 
-if hour < 12:
-    greeting = "Bom dia!"
-elif hour < 18:
-    greeting = "Boa tarde!"
+if hora < 12:
+    saudacao = "Bom dia!"
+elif hora < 18:
+    saudacao = "Boa tarde!"
 else:
-    greeting = "Boa noite!"
+    saudacao = "Boa noite!"
 
 requisicao = requests.get(link)
 requisicao_dic = requisicao.json()
 descricao = requisicao_dic['weather'][0]['description']
 temperatura = requisicao_dic['main']['temp'] - 273.15
-print(f"{format(greeting)} A previsão do tempo hoje em {cidade} é {descricao}, com a temperatura de {temperatura}ºC")
+print(f"{format(saudacao)} A previsão do tempo hoje em {cidade} é {descricao}, com a temperatura de {round(temperatura)}ºC")
