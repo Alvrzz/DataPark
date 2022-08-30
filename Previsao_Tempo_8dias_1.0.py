1b66a8932e8f773cb83ebfcfa12326d19808d19e
William Justino <104076998+Matrix0112358@users.noreply.github.com>
2022-08-30 19:36:59 -0300

    Aplicativo previsao do tempo 1.0 em portugues

--- Previsao_Tempo_8dias_1.0.py
+++ Previsao_Tempo_8dias_1.0.py
@@ -1,53 +1,52 @@
 
 from bs4 import BeautifulSoup as bs
-import matplotlib.pyplot as plt
 import requests
 
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36"
 # US english
 LANGUAGE = "en-US,en;q=0.5"
 
-def get_weather_data(url):
+def obtem_dados_tempo(url):
     session = requests.Session()
     session.headers['User-Agent'] = USER_AGENT
     session.headers['Accept-Language'] = LANGUAGE
     session.headers['Content-Language'] = LANGUAGE
     html = session.get(url)
-    # create a new soup
-    soup = bs(html.text, "html.parser")
-    # store all results on this dictionary
-    result = {}
-    # extract region
-    result['region'] = soup.find("div", attrs={"id": "wob_loc"}).text
-    # extract temperature now
-    result['temp_now'] = soup.find("span", attrs={"id": "wob_tm"}).text
-    # get the day and hour now
-    result['dayhour'] = soup.find("div", attrs={"id": "wob_dts"}).text
-    # get the actual weather
-    result['weather_now'] = soup.find("span", attrs={"id": "wob_dc"}).text
-    # get the precipitation
-    result['precipitation'] = soup.find("span", attrs={"id": "wob_pp"}).text
-    # get the % of humidity
-    result['humidity'] = soup.find("span", attrs={"id": "wob_hm"}).text
-    # extract the wind
-    result['wind'] = soup.find("span", attrs={"id": "wob_ws"}).text
-    # get next few days' weather
-    next_days = []
-    days = soup.find("div", attrs={"id": "wob_dp"})
+    # Criar uma nova sopa
+    sopa = bs(html.text, "html.parser")
+    # Salvar os dados em esta biblioteca 
+    resultado = {}
+    # Extrai a região
+    resultado['region'] = sopa.find("div", attrs={"id": "wob_loc"}).text
+    # Extrai a temperatura agora
+    resultado['temp_now'] = sopa.find("span", attrs={"id": "wob_tm"}).text
+    # Obtem o dia e a hora agora
+    resultado['dayhour'] = sopa.find("div", attrs={"id": "wob_dts"}).text
+    # Obtem a previsao agora
+    resultado['weather_now'] = sopa.find("span", attrs={"id": "wob_dc"}).text
+    # Obtem a precipitacão
+    resultado['precipitation'] = sopa.find("span", attrs={"id": "wob_pp"}).text
+    # Obtem a % de umidade
+    resultado['humidity'] = sopa.find("span", attrs={"id": "wob_hm"}).text
+    # Obtem o vento 
+    resultado['wind'] = sopa.find("span", attrs={"id": "wob_ws"}).text
+    # Obtem a previsao dos proximos dias
+    proximos_dias = []
+    days = sopa.find("div", attrs={"id": "wob_dp"})
     for day in days.findAll("div", attrs={"class": "wob_df"}):
-        # extract the name of the day
-        day_name = day.findAll("div")[0].attrs['aria-label']
-        # get weather status for that day
-        weather = day.find("img").attrs["alt"]
-        temp = day.findAll("span", {"class": "wob_t"})
-        # maximum temparature in Celsius, use temp[1].text if you want fahrenheit
-        max_temp = temp[0].text
-        # minimum temparature in Celsius, use temp[3].text if you want fahrenheit
-        min_temp = temp[2].text
-        next_days.append({"name": day_name, "weather": weather, "max_temp": max_temp, "min_temp": min_temp})
-    # append to result
-    result['next_days'] = next_days
-    return result
+        # Obtem o nome do dia
+        nome_do_dia = day.findAll("div")[0].attrs['aria-label']
+        # Obtem o status para o dia
+        tempo = day.find("img").attrs["alt"]
+        temperatura = day.findAll("span", {"class": "wob_t"})
+        # Tempetarua maxima em Celsius, use temp[1].si voce quiser em fahrenheit
+        max_temp = temperatura[0].text
+        # Temparature minima em Celsius, use temp[3].si voce quiser em fahrenheit
+        min_temp = temperatura[2].text
+        proximos_dias.append({"name": nome_do_dia, "weather": tempo, "max_temp": max_temp, "min_temp": min_temp})
+    # Acrescenta no resultado
+    resultado['next_days'] = proximos_dias
+    return resultado
     
 
 if __name__ == "__main__":
@@ -56,46 +55,25 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Quick Script for Extracting Weather data using Google Weather")
     parser.add_argument("region", nargs="?", help="""Region to get weather for, must be available region.
                                         Default is your current location determined by your IP Address""", default="")
-    # parse arguments
+    # Analisa os argumentos
     args = parser.parse_args()
-    region = args.region
-    if region:
-        region = region.replace(" ", "+")
-        URL += f"+{region}"
-    # get data
-    data = get_weather_data(URL)
-    # print data
-    # print("Tempo em:", data["region"])
-    # print("Hoje:", data["dayhour"])
-    # print(f"Temperatura agora: {data['temp_now']}°C")
-    # print("Descrição:", data['weather_now'])
-    # print("Precipitacão:", data["precipitation"])
-    # print("Umidade:", data["humidity"])
-    # print("Vento:", data["wind"])
-    # print("Proximos dias:")
-    for dayweather in data["next_days"]:
-        # print("="*40, dayweather["name"], "="*40)
-        # print("Descrição:", dayweather["weather"])
-        # print(f"Temperatura Maxima: {dayweather['max_temp']}°C")
-        # print(f"Temperatura Minima: {dayweather['min_temp']}°C")
-        X1 = dayweather['name'] 
-        Y1 = dayweather['max_temp']
-        # plotting the first plot
-        plt.plot(X1, Y1, label = "plot 1") 
-       # Declaring the points for second line plot
-        X2 = dayweather['name']
-        Y2 = dayweather['min_temp']
-        # plotting the second plot 
-        plt.plot(X2, Y2, label = "plot 2") 
-  
-# Labeling the X-axis 
-plt.xlabel('X-axis') 
-# Labeling the Y-axis 
-plt.ylabel('Y-axis') 
-# Give a title to the graph
-plt.title('Two plots on the same graph') 
-  
-# Show a legend on the plot 
-plt.legend() 
- 
-plt.show()
+    regiao = args.region
+    if regiao:
+        regiao = regiao.replace(" ", "+")
+        URL += f"+{regiao}"
+    # Obtem os dados
+    dados = obtem_dados_tempo(URL)
+    # Imprimi os dados
+    print("Tempo em:", dados["region"])
+    print("Hoje:", dados["dayhour"])
+    print(f"Temperatura agora: {dados['temp_now']}°C")
+    print("Descrição:", dados['weather_now'])
+    print("Precipitacão:", dados["precipitation"])
+    print("Umidade:", dados["humidity"])
+    print("Vento:", dados["wind"])
+    print("Proximos dias:")
+    for tempododia in dados["next_days"]:
+        print("="*40, tempododia["name"], "="*40)
+        print("Descrição:", tempododia["weather"])
+        print(f"Temperatura Maxima: {tempododia['max_temp']}°C")
+        print(f"Temperatura Minima: {tempododia['min_temp']}°C")
