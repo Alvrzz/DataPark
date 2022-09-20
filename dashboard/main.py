fbb203480adb755bdc8970d865fa3d3a4942c7b1
Luan Alvarez <alvarezluan.ti@gmail.com>
2022-09-20 20:15:06 -0300

    resolução do erro

--- dashboard/main.py
+++ dashboard/main.py
@@ -5,110 +5,105 @@ from lxml import html
 from bs4 import BeautifulSoup as bs
 import requests
 
-# Connecta ao banco de dados.
-conn = mysql.connector.connect(
-    host = '3.89.36.150',
-    user = 'e2122g4',
-    password = 'e2122g4@16@ago',
-    database = 'e2122g4'
-    )
-c = conn.cursor()
+def previsao():
+    # Connecta ao banco de dados.
+    conn = mysql.connector.connect(
+        host = '3.89.36.150',
+        user = 'e2122g4',
+        password = 'e2122g4@16@ago',
+        database = 'e2122g4'
+        )
+    c = conn.cursor()
 
-USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36"
-# Ingles Americano.
-LANGUAGE = "en-US,en;q=0.5"
+    USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36"
+    # Ingles Americano.
+    LANGUAGE = "en-US,en;q=0.5"
 
-# Define especificamente cada dado no qual sera extraido.
-def obtem_dados_tempo(url):
-    session = requests.Session()
-    session.headers['User-Agent'] = USER_AGENT
-    session.headers['Accept-Language'] = LANGUAGE
-    session.headers['Content-Language'] = LANGUAGE
-    html = session.get(url)
-    # Criar uma nova sopa.
-    sopa = bs(html.text, "html.parser")
-    # Salvar os dados em esta biblioteca. 
-    resultado = {}
-    # Extrai a região.
-    resultado['region'] = sopa.find("div", attrs={"id": "wob_loc"}).text
-    # Extrai a temperatura agora.
-    resultado['temp_now'] = sopa.find("span", attrs={"id": "wob_tm"}).text
-    # Obtem o dia e a hora agora.
-    resultado['dayhour'] = sopa.find("div", attrs={"id": "wob_dts"}).text
-    # Obtem a previsao agora.
-    resultado['weather_now'] = sopa.find("span", attrs={"id": "wob_dc"}).text
-    # Obtem a previsao dos proximos dias.
-    proximos_dias = []
-    days = sopa.find("div", attrs={"id": "wob_dp"})
-    for day in days.findAll("div", attrs={"class": "wob_df"}):
-        # Obtem o nome do dia.
-        nome_do_dia = day.findAll("div")[0].attrs['aria-label']
-        # Obtem o status para o dia.
-        tempo = day.find("img").attrs["alt"]
-        temperatura = day.findAll("span", {"class": "wob_t"})
-        # Tempetarua maxima em Celsius, use temp[1].si voce quiser em fahrenheit.
-        max_temp = temperatura[0].text
-        # Temparature minima em Celsius, use temp[3].si voce quiser em fahrenheit.
-        min_temp = temperatura[2].text
-        proximos_dias.append({"name": nome_do_dia, "weather": tempo, "max_temp": max_temp, "min_temp": min_temp})
-    # Acrescenta no resultado.
-    resultado['next_days'] = proximos_dias
-    return resultado
-    
-    # Pagina de extracao do dia das semana, temperatura maxima e temperatura minima.
-if __name__ == "__main__":
+    # Define especificamente cada dado no qual sera extraido.
+    def obtem_dados_tempo(url):
+        session = requests.Session()
+        session.headers['User-Agent'] = USER_AGENT
+        session.headers['Accept-Language'] = LANGUAGE
+        session.headers['Content-Language'] = LANGUAGE
+        html = session.get(url)
+        # Criar uma nova sopa.
+        sopa = bs(html.text, "html.parser")
+        # Salvar os dados em esta biblioteca. 
+        resultado = {}
+        # Extrai a região.
+        resultado['region'] = sopa.find("div", attrs={"id": "wob_loc"}).text
+        # Extrai a temperatura agora.
+        resultado['temp_now'] = sopa.find("span", attrs={"id": "wob_tm"}).text
+        # Obtem o dia e a hora agora.
+        resultado['dayhour'] = sopa.find("div", attrs={"id": "wob_dts"}).text
+        # Obtem a previsao agora.
+        resultado['weather_now'] = sopa.find("span", attrs={"id": "wob_dc"}).text
+        # Obtem a previsao dos proximos dias.
+        proximos_dias = []
+        days = sopa.find("div", attrs={"id": "wob_dp"})
+        for day in days.findAll("div", attrs={"class": "wob_df"}):
+            # Obtem o nome do dia.
+            nome_do_dia = day.findAll("div")[0].attrs['aria-label']
+            # Obtem o status para o dia.
+            tempo = day.find("img").attrs["alt"]
+            temperatura = day.findAll("span", {"class": "wob_t"})
+            # Tempetarua maxima em Celsius, use temp[1].si voce quiser em fahrenheit.
+            max_temp = temperatura[0].text
+            # Temparature minima em Celsius, use temp[3].si voce quiser em fahrenheit.
+            min_temp = temperatura[2].text
+            proximos_dias.append({"name": nome_do_dia, "weather": tempo, "max_temp": max_temp, "min_temp": min_temp})
+        # Acrescenta no resultado.
+        resultado['next_days'] = proximos_dias
+        return resultado
+        
+        # Pagina de extracao do dia das semana, temperatura maxima e temperatura minima.
+   
     URL = "https://www.google.com/search?q=weather+blumenau&lr=lang_en&tbs=lr%3Alang_1en&sxsrf=ALiCzsaNKbuNAdCkdBIf_hZHbYCeppweEQ%3A1660566440526&ei=qDv6YsreH-LQ1sQP1b-zwAk&oq=wetaher+bluem&gs_lcp=Cgdnd3Mtd2l6EAEYADIJCAAQDRBGEIACMgYIABAeEA0yBggAEB4QDTIICAAQHhANEAoyBggAEB4QDTIGCAAQHhAWMgYIABAeEBYyBggAEB4QFjIICAAQHhAWEAoyBggAEB4QFjoMCCMQJxCdAhBGEIACOgQIIxAnOgQIABBDOggIABCABBCxAzoLCC4QgAQQxwEQ0QM6CwgAEIAEELEDEIMBOg4ILhCABBCxAxDHARDRAzoLCC4QgAQQsQMQgwE6CggAELEDEIMBEEM6BwgAELEDEEM6CgguEMcBENEDEEM6CQgAEMkDEAoQQzoFCAAQkgM6DgguEIAEEMcBENEDENQCOgoIABCABBDJAxAKOg0ILhCxAxDHARDRAxAKOgUILhCABDoHCC4QgAQQCjoFCAAQgAQ6BAguEEM6EggjELECEMkDECcQnQIQRhCAAjoKCAAQsQMQgwEQCjoECAAQCjoICAAQsQMQgwE6BwgAELEDEAo6EggAELEDEIMBEMkDEEMQRhCAAjoQCC4QsQMQxwEQ0QMQyQMQCjoECAAQDUoECEEYAEoECEYYAFAAWKQQYNEjaABwAXgAgAGhAogBqw2SAQYwLjEyLjGYAQCgAQHAAQE&sclient=gws-wiz"
     import argparse
     parser = argparse.ArgumentParser(description="Quick Script for Extracting Weather data using Google Weather")
     parser.add_argument("region", nargs="?", help="""Region to get weather for, must be available region.
-                                        Default is your current location determined by your IP Address""", default="")
-    # Analisa os argumentos.
+                                            Default is your current location determined by your IP Address""", default="")
+        # Analisa os argumentos.
     args = parser.parse_args()
     regiao = args.region
     if regiao:
         regiao = regiao.replace(" ", "+")
         URL += f"+{regiao}"
-    # Obtem os dados.
+        # Obtem os dados.
     dados = obtem_dados_tempo(URL)
 
-# define oque sera deletado no banco de dados, neste caso a tabela previsao. 
-def drop_table():
-    c.execute("""DROP TABLE IF EXISTS PREVISAO""")
+    # define oque sera deletado no banco de dados, neste caso a tabela previsao. 
+    def drop_table():
+        c.execute("""DROP TABLE IF EXISTS PREVISAO""")
 
-# define a criacao da  tabela.        
-def create_table():
-    c.execute("""
-    CREATE TABLE PREVISAO(
-        PREVISAO_ID INTEGER PRIMARY KEY AUTO_INCREMENT,
-        DATA DATE NOT NULL,
-        DIA_SEMANA TEXT NOT NULL,
-        TEMP_MAX TEXT NOT NULL, 
-        TEMP_MIN TEXT NOT NULL,
-        MM_CHUVA_PRECI TEXT NOT NULL
-        );
-""")
-
-
-drop_table() # Deleta a tabela.
-create_table() # Cria novamente a tabela.
-
-conn.commit()
-
-# Pagina para extracao do Milimetro de chuva.
-pagina = requests.get('https://www.climatempo.com.br/previsao-do-tempo/15-dias/cidade/372/blumenau-sc')
-arvore = html.fromstring(pagina.text)
-milimetros = arvore.xpath('//span[@class="_margin-l-5"]/text()')  
-
-def web_scraping():
- global x
- global y
- y = -1 # Junto com o y+=1 e mm_chuva = (milimetros[y]) adiciona o milimetros de chuva no restante dos dias da semana no banco de dados.
- x = -1 # Junto com o x+=1 e  a = dia_hj + timedelta.Timedelta(days=x) adiciona o restante das datas dos dias da semana no banco de dados.
- dia_hj = datetime.date.today()
+    # define a criacao da  tabela.        
+    def create_table():
+        c.execute("""
+        CREATE TABLE PREVISAO(
+            PREVISAO_ID INTEGER PRIMARY KEY AUTO_INCREMENT,
+            DATA DATE NOT NULL,
+            DIA_SEMANA TEXT NOT NULL,
+            TEMP_MAX TEXT NOT NULL, 
+            TEMP_MIN TEXT NOT NULL,
+            MM_CHUVA_PRECI TEXT NOT NULL
+            );
+    """)
 
 
+    drop_table() # Deleta a tabela.
+    create_table() # Cria novamente a tabela.
 
- for tempododia in dados["next_days"]:  
+    conn.commit()
+
+    # Pagina para extracao do Milimetro de chuva.
+    pagina = requests.get('https://www.climatempo.com.br/previsao-do-tempo/15-dias/cidade/372/blumenau-sc')
+    arvore = html.fromstring(pagina.text)
+    milimetros = arvore.xpath('//span[@class="_margin-l-5"]/text()')  
+
+    y = -1 # Junto com o y+=1 e mm_chuva = (milimetros[y]) adiciona o milimetros de chuva no restante dos dias da semana no banco de dados.
+    x = -1 # Junto com o x+=1 e  a = dia_hj + timedelta.Timedelta(days=x) adiciona o restante das datas dos dias da semana no banco de dados.
+    dia_hj = datetime.date.today()
+    for tempododia in dados["next_days"]:  
         nome_dia = tempododia['name']
         max = tempododia['max_temp']
         min = tempododia['min_temp']
@@ -120,13 +115,8 @@ def web_scraping():
         sql = ('INSERT INTO PREVISAO ( DATA, DIA_SEMANA,TEMP_MAX,TEMP_MIN, MM_CHUVA_PRECi) VALUES (%s, %s, %s, %s, %s)')
         val = a, nome_dia, max, min, str(mm_chuva)
         c.execute(sql, val)
-
-        
         conn.commit()
         
-
-web_scraping()
     
-   
 
-conn.close()
+    conn.close()
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from dashboard.main import obtem_dados_tempo, drop_table, create_table, web_scraping
+from dashboard.main import previsao
 from limpeza.arquivos.limpeza_dados import limpeza1
 from limpeza.arquivos.limpeza_previsao import limpeza2
 
@@ -8,18 +8,11 @@ from limpeza.arquivos.limpeza_previsao import limpeza2
 #EnviaDatabase()
 #print('Database enviado com sucesso.')
 
-limpeza1()
-print('Limpeza e exportação  da base de dados concluida\n')
+#limpeza1()
+#print('Limpeza e exportação  da base de dados concluida\n')
 
-obtem_dados_tempo()
-print('Dados da previsão do tempo obtidos com sucesso!\n')
+previsao()
+print('Previsão enviada com sucesso')
 
-drop_table()
-create_table()
-print('Tabela criada com sucesso.\n')
-
-web_scraping()
-print('Dados da previsão inseridos com sucesso nas tabelas.\n')
-
-limpeza2()
-print('Limpeza e exportação da base de previsão concluída.\n')
+#limpeza2()
+#print('Limpeza e exportação da base de previsão concluída.\n')
