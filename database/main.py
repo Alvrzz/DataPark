af259dfcf0b76161cb22be743e83146e8b3e3d26
Gabriel Cuco Mello <40546046+Gabreelzo@users.noreply.github.com>
2022-09-27 19:19:59 -0300

    arrumado database

--- database/main.py
+++ database/main.py
@@ -8,7 +8,7 @@ from database.chuva_clientes.data_total import data_mult
 from database.atracoes.atracoes import atracoes, setor
 from database.funcionarios.data_mult import data_mult_funcionarios
 from database.funcionarios.funcionarios import funcionarios
-import mysql.connector,sqlite3
+import mysql.connector
 import pandas as pd
 
 def GeraDatabase():
@@ -28,15 +28,14 @@ def GeraDatabase():
     passos_atracao = 10
     passos_funcionarios = 10000
 
-    cnx = sqlite3.connect('DATABASE.sqlite3')
 
     # Conexão com o database
-    # cnx = mysql.connector.connect(
-    #     host = '3.89.36.150',
-    #     user = 'e2122g4',
-    #     password = 'e2122g4@16@ago',
-    #     database = 'e2122g4'
-    #     )
+    cnx = mysql.connector.connect(
+        host = '3.89.36.150',
+        user = 'e2122g4',
+        password = 'e2122g4@16@ago',
+        database = 'e2122g4'
+        )
 
     cur = cnx.cursor()
 
@@ -47,7 +46,7 @@ def GeraDatabase():
 
     cur.execute('''
         CREATE TABLE CLIENTES(
-            CLIENTE_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+            CLIENTE_ID INTEGER PRIMARY KEY AUTO_INCREMENT,
             CLIENTE_NOME TEXT NOT NULL,
             CLIENTE_IDADE INTEGER NOT NULL,
             CLIENTE_DATA DATE NOT NULL,
@@ -62,7 +61,7 @@ def GeraDatabase():
 
     cur.execute('''
         CREATE TABLE CLIMA(
-            CLIMA_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+            CLIMA_ID INTEGER PRIMARY KEY AUTO_INCREMENT,
             CLIMA_DATA DATE NOT NULL,
             CLIMA_CHUVA_MM INTEGER NOT NULL
         );
@@ -74,7 +73,7 @@ def GeraDatabase():
 
     cur.execute('''
         CREATE TABLE ATRACOES(
-            ATRACAO_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+            ATRACAO_ID INTEGER PRIMARY KEY AUTO_INCREMENT,
             ATRACAO_NOME TEXT NOT NULL,
             ATRACAO_SETOR TEXT NOT NULL
         );
@@ -87,7 +86,7 @@ def GeraDatabase():
 
     cur.execute('''
         CREATE TABLE FUNCIONARIOS(
-            FUNCIONARIO_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+            FUNCIONARIO_ID INTEGER PRIMARY KEY AUTO_INCREMENT,
             FUNCIONARIO_NOME TEXT NOT NULL,
             FUNCIONARIO_DATA DATE NOT NULL,
             FUNCIONARIO_ATRACAO_ID INTEGER NOT NULL
@@ -100,7 +99,7 @@ def GeraDatabase():
 
     cur.execute("""    
         CREATE TABLE CIDADES(
-                CIDADE_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                CIDADE_ID INTEGER PRIMARY KEY AUTO_INCREMENT,
                 CIDADE_UF TEXT NOT NULL,
                 CIDADE_NOME TEXT NOT NULL
         );
@@ -114,28 +113,28 @@ def GeraDatabase():
 
     # Envia todas tuplas para o database na nuvem
     while contagem_clientes < sum(clientes):
-        cur.executemany('INSERT INTO CLIENTES (CLIENTE_NOME, CLIENTE_IDADE, CLIENTE_DATA, CLIENTE_CPF, CLIENTE_CIDADE_ID) VALUES (?, ?, ?, ?, ?)', clientes_final[contagem_clientes: passos_clientes])
+        cur.executemany('INSERT INTO CLIENTES (CLIENTE_NOME, CLIENTE_IDADE, CLIENTE_DATA, CLIENTE_CPF, CLIENTE_CIDADE_ID) VALUES (%s, %s, %s, %s, %s)', clientes_final[contagem_clientes: passos_clientes])
         contagem_clientes += 100000
         passos_clientes += 100000
         cnx.commit()
         print('Adição de 100 mil registros de CLIENTES completa.')
 
     while contagem_clima < len(clientes_final):
-        cur.executemany('INSERT INTO CLIMA (CLIMA_DATA, CLIMA_CHUVA_MM) VALUES (?, ?)', clima_final[contagem_clima: passos_clima])
+        cur.executemany('INSERT INTO CLIMA (CLIMA_DATA, CLIMA_CHUVA_MM) VALUES (%s, %s)', clima_final[contagem_clima: passos_clima])
         contagem_clima += 100
         passos_clima += 100
         cnx.commit()
         print('Adição de 100 registros de CHUVA completa.')
 
     while contagem_atracao < len(atracoes):
-        cur.executemany('INSERT INTO ATRACOES (ATRACAO_NOME, ATRACAO_SETOR) VALUES (?, ?)', atracao_final[contagem_atracao: passos_atracao])
+        cur.executemany('INSERT INTO ATRACOES (ATRACAO_NOME, ATRACAO_SETOR) VALUES (%s, %s)', atracao_final[contagem_atracao: passos_atracao])
         contagem_atracao += 10
         passos_atracao += 10
         cnx.commit()
         print('Adição de 10 registros de ATRACOES completa.')
 
     while contagem_funcionarios < len(data_mult_funcionarios):
-        cur.executemany('INSERT INTO FUNCIONARIOS (FUNCIONARIO_NOME, FUNCIONARIO_DATA, FUNCIONARIO_ATRACAO_ID) VALUES (?, ?, ?)', funcionarios_final[contagem_funcionarios: passos_funcionarios])
+        cur.executemany('INSERT INTO FUNCIONARIOS (FUNCIONARIO_NOME, FUNCIONARIO_DATA, FUNCIONARIO_ATRACAO_ID) VALUES (%s, %s, %s)', funcionarios_final[contagem_funcionarios: passos_funcionarios])
         contagem_funcionarios += 10000
         passos_funcionarios += 10000
         cnx.commit()
@@ -144,7 +143,7 @@ def GeraDatabase():
     cidades = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSqpxiXuROqjtAI24wP7WSLvg6YlwOHKfQASvXZ3I-zRmxCW6Q2oDx_IG8uT0rdIBUxomunDyURxW-G/pub?gid=385229407&single=true&output=csv'
     colunas = list(['ID','UF', 'Município'])
     df = pd.read_csv(cidades, index_col=0, header=0, usecols=colunas)
-    sql=("INSERT INTO CIDADES (CIDADE_UF, CIDADE_NOME) VALUES (?, ?) ")
+    sql=("INSERT INTO CIDADES (CIDADE_UF, CIDADE_NOME) VALUES (%s, %s) ")
     for index, row in df.iterrows():
         val=(row.UF, row.Município)
         cur.execute(sql,val)
